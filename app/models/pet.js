6006bd40d80788d6736fc04c14e087e91b886679
NikPurvis <nikpurvis@gmail.com>
2022-03-30 07:29:46 -0600

    Created pet model

---
+++ app/models/pet.js
@@ -0,0 +1,41 @@
+// PET -> has many toys and has owner that is user
+
+const mongoose = require ("mongoose")
+
+const { Schema, model } = mongoose
+
+const petSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        },
+        adoptable: {
+            type: Boolean,
+            required: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },{
+        timestamps: true,
+        // We're going to add virtuals to our model.
+        // These lines ensure that the virtual will be included whenever we turn our document to an object or JSON.
+        toObject: { virtuals: true },
+        toJSON: { virtuals: true }
+    }        
+
+)
+
+// Virtuals go here (we'll build these later)
+
+model.exports = model("Pet", petSchema)
